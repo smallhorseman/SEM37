2f066a2c1db06e9aa3fae8b248c01c1e793620b4
ceo <ceo@ponyboy.win>
2025-08-27 05:57:25 +0000

    u

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -7,9 +7,9 @@
   # Use https://search.nixos.org/packages to find packages
   packages = [
     # pkgs.go
-    # pkgs.python311
+    pkgs.python311
     # pkgs.python311Packages.pip
-    # pkgs.nodejs_20
+    pkgs.nodejs_20
     # pkgs.nodePackages.nodemon
   ];
 
